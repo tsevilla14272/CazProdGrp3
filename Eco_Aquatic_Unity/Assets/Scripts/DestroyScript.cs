60fcaea450a4fa09b8038119c2116c05502d2e7a
tsevilla14272 <55515847+tsevilla14272@users.noreply.github.com>
2019-10-08 20:46:07 -0400

    Level1 First Commit

---
+++ Eco_Aquatic_Unity/Assets/Scripts/DestroyScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour
+{
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Bubble")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Eco_Aquatic_Unity/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public GameObject bubble;
+    public float delayTime;
+    bool canShoot = true;
+    
+    //shooting
+    void Update()
+    {
+     
+        if (canShoot && Input.GetMouseButton(1))
+        {
+            canShoot = false;
+            Instantiate(bubble, transform.position, transform.rotation);
+            StartCoroutine(NoFire());
+        }
+    }
+
+    IEnumerator NoFire()
+    {
+        yield return new WaitForSeconds(delayTime);
+        canShoot = true;
+    }
+}
